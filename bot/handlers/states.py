bef27d420269b046d122197fbed2a9d78702c4aa
vladislav.hungureev <vladislav.hungureev@parovoz.tv>
2023-09-28 15:52:20 +0300

    Merged

--- bot/handlers/states.py
+++ bot/handlers/states.py
@@ -1,7 +1,14 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class UserStates(StatesGroup):
-    choose_category = State() 
-    choosing_category = State()  # выбор категории
+class States(StatesGroup):
+    CHOOSE_CATEGORY = State()
+    CHOOSING_CATEGORY = State()  # выбор категории
 
+    SHOW_RECIPE = State()
+    SHOW_CATEGORIES = State()
+    SHOW_INGREDIENTS = State()
+    SHOW_DISHES = State()
+    # ADD_FAVORITES = State()
+    SHOW_FAVORITES = State()
+    SHOW_PAY = State()
--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -1,11 +1,10 @@
 from aiogram import Bot, Router
-from aiogram.filters import CommandStart, Text
 from bot.config_data.config import load_config
-from aiogram.filters import CommandStart, Text
+from aiogram.filters import CommandStart, Command, Text
 from aiogram.types import Message, FSInputFile
 from aiogram.fsm.context import FSMContext
 from bot.keyboards import user_keyboards
-from bot.handlers.states import UserStates
+from bot.handlers.states import States
 
 
 router = Router()
@@ -23,4 +22,41 @@ async def process_start_command(message: Message, state: FSMContext):
     user_id = int(message.from_user.id)
     await state.update_data(prods=[])
     await state.update_data(user_id=user_id)
-    await state.set_state(UserStates.choosing_category)
+    await state.set_state(States.choosing_category)
+
+
+@router.message(Command(commands=["categories", "start"]))
+@router.message(States.SHOW_CATEGORIES)
+async def get_categories(message: Message, state: FSMContext):
+    await message.answer("Categories")
+    await state.set_state(States.SHOW_DISHES)
+
+
+@router.message(States.SHOW_DISHES)
+async def get_dishes(message: Message, state: FSMContext):
+    await message.answer("Dishes")
+    await state.set_state(States.SHOW_RECIPE)
+
+
+@router.message(States.SHOW_RECIPE)
+async def get_recipe(message: Message, state: FSMContext):
+    await message.answer("Recept")
+    await state.set_state(States.SHOW_INGREDIENTS)
+
+
+@router.message(States.SHOW_INGREDIENTS)
+async def get_ingredients(message: Message, state: FSMContext):
+    await message.answer("ingredients")
+    await state.clear()
+
+
+@router.message(States.SHOW_FAVORITES)
+@router.message(Command(commands=["favorites"]))
+async def get_favorites(message: Message, state: FSMContext):
+    await message.answer("favorites")
+    # await state.set_state(States.SHOW_DISHES) # todo
+
+
+@router.message(States.SHOW_PAY)
+async def get_pay(message: Message, state: FSMContext):
+    await message.answer("pay")
