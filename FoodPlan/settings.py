0a3fa7b3690ec734eb4d7186bc563f8affef2e45
shadowsking <shadowsking1295@gmail.com>
2023-10-01 23:03:42 +0300

    Added get categories and dishes

--- FoodPlan/settings.py
+++ FoodPlan/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'FoodPlan.wsgi.application'
+ASGI_APPLICATION = 'FoodPlan.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
